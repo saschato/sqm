989fa5a3b1333e5b9e5baae4b4d7cb18a906c360
Sascha Tommasone <sascha@tommasone.me>
2023-04-14 18:39:56 +0200

    update

--- python/sqm.py
+++ python/sqm.py
@@ -1,14 +1,19 @@
 def alg(base, power, mod):
+    print()
+
     res = 1
     for i in power[2:]:
         if i == "0":
-            print(f"sq:  {res}^2 = {(res**2) % mod}")
+            print(f" sq:  {res}^2 = {(res**2) % mod}")
             res = (res ** 2) % mod
         elif i == "1":
-            print(f"sqm: {res}^2*{base} = {(res**2*base) % mod}")
+            print(f" sqm: {res}^2*{base} = {(res**2*base) % mod}")
             res = (res ** 2 * base) % mod
-    print(res)
+
+    print("\n", f"result is: {res} mod {mod}")
+    
 
 if __name__ == "__main__":
-    alg(117, bin(438), 1051)
+    print()
+    alg(int(input(" * enter base: ")),bin(int(input(" * enter power: "))), int(input(" * enter mod: ")))
 
